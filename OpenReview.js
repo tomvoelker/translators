06722f91ae35e3e70ebe55eefb195f6fb1d4b9a9
Tom Völker <voelker@informatik.uni-wuerzburg.de>
2026-06-01 06:04:32 +0200

    Handle OpenReview IDs after query params

--- OpenReview.js
+++ OpenReview.js
@@ -2,14 +2,14 @@
 	"translatorID": "096fe0f3-49df-4e63-b8d7-047705034585",
 	"label": "OpenReview",
 	"creator": "Tom Voelker",
-	"target": "^https://openreview\\.net/(?:forum|pdf)\\?id=[A-Za-z0-9_-]+",
+	"target": "^https://openreview\\.net/(?:forum|pdf)\\?(?:[^&#]*&)*id=[A-Za-z0-9_-]+",
 	"minVersion": "5.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2026-06-01 02:14:03"
+	"lastUpdated": "2026-06-01 04:03:41"
 }
 
 /*
@@ -134,6 +134,49 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "https://openreview.net/forum?id=nzpLWnVAyah",
+		"items": [
+			{
+				"itemType": "conferencePaper",
+				"creators": [
+					{
+						"firstName": "Marius",
+						"lastName": "Mosbach",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Maksym",
+						"lastName": "Andriushchenko",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Dietrich",
+						"lastName": "Klakow",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"title": "On the Stability of Fine-tuning BERT: Misconceptions, Explanations, and Strong Baselines",
+				"abstractNote": "Fine-tuning pre-trained transformer-based language models such as BERT has become a common practice dominating leaderboards across various NLP benchmarks. Despite the strong empirical performance of fine-tuned models, fine-tuning is an unstable process: training the same model with multiple random seeds can result in a large variance of the task performance. Previous literature (Devlin et al., 2019; Lee et al., 2020; Dodge et al., 2020) identified two potential reasons for the observed instability: catastrophic forgetting and small size of the fine-tuning datasets. In this paper, we show that both hypotheses fail to explain the fine-tuning instability. We analyze BERT, RoBERTa, and ALBERT, fine-tuned on commonly used datasets from the GLUE benchmark, and show that the observed instability is caused by optimization difficulties that lead to vanishing gradients. Additionally, we show that the remaining variance of the downstream task performance can be attributed to differences in generalization where fine-tuned models with the same training loss exhibit noticeably different test performance. Based on our analysis, we present a simple but strong baseline that makes fine-tuning BERT-based models significantly more stable than the previously proposed approaches. Code to reproduce our results is available online: https://github.com/uds-lsv/bert-stable-fine-tuning.",
+				"conferenceName": "International Conference on Learning Representations",
+				"date": "2020/10/02",
+				"url": "https://openreview.net/forum?id=nzpLWnVAyah",
+				"language": "en",
+				"libraryCatalog": "openreview.net",
+				"shortTitle": "On the Stability of Fine-tuning BERT"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://openreview.net/pdf?foo=1&id=nzpLWnVAyah",
 		"items": [
 			{
 				"itemType": "conferencePaper",
